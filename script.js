ac8b1891c6ca733333f377a87ed47a84bafa32a0
Dan Green <dan.ryan.green@gmail.com>
2020-12-29 21:18:24 -0700

    Continued adding divs to home page

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var countdown = 100;
+var score = 0;
 
 var timerElement = document.getElementById("timer");
 timerElement.innerHTML = "Time: " + countdown;
+
+var scoreElement = document.getElementById("score");
+scoreElement.innerHTML = "Your score is: " + score;
